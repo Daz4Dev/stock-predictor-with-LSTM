1b0b595ccde04d9134ace61cdd207425478f6fea
Daz4Dev <147746778+Daz4Dev@users.noreply.github.com>
2024-02-10 15:47:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as pdr
+
+from keras.models import load_model
+import streamlit as st
+
+start = '2010-01-01'
+end = '2022-12-31'
+
+
+#creating dataframe
+st.title('stock trend predictor')
+user_input = st.text_input('Enter Stock Ticker','AMZN')
+df = pdr.DataReader(user_input, 'stooq', start, end)                #use stooq
+reversed_df = df.iloc[::-1]                         #reverse
+df=reversed_df
+
+#describing data
+st.subheader('Data from 2010-2022')
+st.write(df.describe())
+
+#visualizing data
+st.subheader('Closing Price vs Time chart')
+fig = plt.figure(figsize=(12,6))
+plt.plot(df['Close'])
+st.pyplot(fig)
+
+#moving averages
+st.subheader('Predicted Close Price with 100MA and 200MA')
+ma100 = df.Close.rolling(100).mean()
+ma200 = df.Close.rolling(200).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close,'b')
+plt.plot(ma100,'g')
+plt.plot(ma200,'r')
+st.pyplot(fig)
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])                 
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70):int(len(df))])
+
+print(data_training.shape)
+print(data_testing.shape)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+data_training_array = scaler.fit_transform(data_training)
+
+x_train = []
+y_train = []
+
+for i in range(100, data_training_array.shape[0]):
+    x_train.append(data_training_array[i-100:i])
+    y_train.append(data_training_array[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+#load model
+model = load_model('model2.h5')
+
+#testing
+past_100_days = data_training.tail(100)
+final_df = pd.concat([past_100_days,data_testing], ignore_index = True)      #concat instead of append
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+
+for i in range(100, input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+
+scaler = scaler.scale_
+scale_factor = 1/scaler[0]
+y_predicted_scaled = y_predicted*scale_factor
+y_test_scaled = y_test*scale_factor
+
+#final graph
+st.subheader('predictions vs original')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test_scaled, 'b', label = 'Original Price')
+plt.plot(y_predicted_scaled, 'r', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+st.pyplot(fig2)
+#plt.show()
